2988f789d400440bd56ca64fb30075256e330f05
Zach Mueller <zach.a.mueller@gmail.com>
2015-05-31 17:27:08 -0500

    5/31/15 update

--- js/indexScript.js
+++ js/indexScript.js
@@ -8,22 +8,27 @@ $(document).ready(function () {
     function fadeOutArea(area) {
         $(area).fadeOut(500);
     }
-    
+    function foreverLoop() {
+        setInterval(function () { $("#leftImg").delay(500).fadeIn(500).delay(350).fadeOut(500).delay(5000);
+            $("#rightImg").delay(500).fadeIn(500).delay(350).fadeOut(500).delay(5000); }, 3000);
+
+    }
     
     $("#left-back").click(function () {
         $(this).css('background', '#000000');
     });
     
     $("#right-back").hover(function () {
-        $("#right-list").delay(50).fadeIn(500);
+        $("#right-list").delay(630).fadeIn(500);
     }, function () {
-        $("#right-list").delay(5).fadeOut(500);
+        $("#right-list").delay(0).fadeOut(100);
     });
     
     
     
-    $("#leftImg").hide().delay(500).fadeIn(500).delay(350).fadeOut(500);
-    $("#rightImg").hide().delay(500).fadeIn(500).delay(350).fadeOut(500);
+    $("#leftImg").hide();
+    $("#rightImg").hide();
+    foreverLoop();
     
     
     
